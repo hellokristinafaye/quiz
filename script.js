a94561a45054e62f59712454fc1ca4eabdfda73d
Kristina Faye <hellokristinafaye@gmail.com>
2024-09-04 15:55:50 -0700

    added questions and answers

--- script.js
+++ script.js
@@ -1,1 +1,29 @@
-console.log("Hello world!");
+const questions = [
+    {
+        question: "Who won Wedding Cakes?",
+        answers: [
+            {text: "Keith", correct: false},
+            {text: "Eugene", correct: false},
+            {text: "Marissa", correct: true},
+            {text: "Zach", correct: false},
+        ]
+    },
+    {
+        question: "Who won Illusion Cakes?",
+        answers: [
+            {text: "Keith", correct: true},
+            {text: "Eugene", correct: false},
+            {text: "Marissa", correct: false},
+            {text: "Zach", correct: false},
+        ]
+    },
+    {
+        question: "Who won Sandwiches?",
+        answers: [
+            {text: "Keith", correct: false},
+            {text: "Eugene", correct: false},
+            {text: "Marissa", correct: false},
+            {text: "Zach", correct: true},
+        ]
+    },
+];
