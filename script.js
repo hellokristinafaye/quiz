70808315c91bf878cabe23ad51eb71ec0775f46a
Kristina Faye <hellokristinafaye@gmail.com>
2024-09-04 15:59:17 -0700

    added variables to grab HTML elements

--- script.js
+++ script.js
@@ -27,3 +27,7 @@ const questions = [
         ]
     },
 ];
+
+const questionElement = document.getElementById("question")
+const answerButton = document.getElementById("answer-buttons")
+const nextButton = document.getElementById("next-btn")
